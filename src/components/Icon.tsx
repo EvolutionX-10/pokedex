e24276f8afe3cf6a2366723804bca3b56cbe0aaf
EvolutionX <evolutionx9777@gmail.com>
2023-05-09 21:50:22 +0530

    feat: add hover animation on icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,6 +1,6 @@
 export function Icon({ type }: IIcon) {
 	return (
-		<div className={`icon ${type}`}>
+		<div className={`icon ${type}`} data-type={type}>
 			<img src={`assets/icons/${type}.svg`} alt={type} />
 		</div>
 	);
