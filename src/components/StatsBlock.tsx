5612b178dbbc454b28c183e733900f9917e4e244
EvolutionX <evolutionx9777@gmail.com>
2023-07-26 18:14:34 +0530

    chore: fix height

--- src/components/StatsBlock.tsx
+++ src/components/StatsBlock.tsx
@@ -12,7 +12,7 @@ export function StatsBlock({ stats }: StatsBlockProps) {
 	};
 
 	return (
-		<div className="min-w-[min(16rem,80vw)] max-w-[min(20rem,90vw)] flex flex-col items-center justify-around rounded-md border-2 border-blue-400 px-4">
+		<div className="h-[250px] flex min-w-[min(16rem,80vw)] max-w-[min(20rem,90vw)] flex-col items-center justify-around rounded-md border-2 border-blue-400 px-4">
 			<h2 className="pb-2 text-3xl font-medium">Base Stats</h2>
 			{Object.entries(stats).map(([key, value]) => {
 				return <Bar key={key} color={color[key]} stat={value} name={key} />;
