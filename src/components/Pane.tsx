7b4c5c16d99c7d661e03f4bd1a06214bcb5c414c
EvolutionX <evolutionx9777@gmail.com>
2023-08-03 10:17:03 +0530

    chore: c&p fail
    permalink: http://whatthecommit.com/063bdaeedaecb92c1557897459978761

--- src/components/Pane.tsx
+++ src/components/Pane.tsx
@@ -5,9 +5,8 @@ import { Pill } from './Pill';
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.num}.svg
 export function Pane({ pokemon }: IPane) {
 	if (!pokemon) return <div></div>;
-	const pills = pokemon.types.map((r) => (
-		<Pill type={r.name.toLowerCase()} />
-	));
+	const pills = pokemon.types.map((r) => <Pill type={r.name.toLowerCase()} />);
+
 	return (
 		<div
 			className="m-5 flex h-40 w-[min(26rem,90vw)] cursor-pointer items-center justify-between rounded-2xl bg-gray-100 shadow-[0_0_10px_rgb(99,99,99)] hover:shadow-[0_0_15px_rgb(80,80,80)]"
@@ -16,9 +15,7 @@ export function Pane({ pokemon }: IPane) {
 			}}
 		>
 			<div className="flex w-2/5 flex-col-reverse items-center justify-center gap-7">
-				<strong className="text-2xl capitalize">
-					{pokemon.species}
-				</strong>
+				<strong className="text-2xl capitalize">{pokemon.species}</strong>
 				<div className="info-pills">{...pills}</div>
 			</div>
 			<img
