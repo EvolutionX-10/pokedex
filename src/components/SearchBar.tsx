b17b47fbcfe8bed5c721edb5edbaf3b0cc765338
EvolutionX <evolutionx9777@gmail.com>
2023-08-27 10:00:00 +0530

    chore: progress

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,8 @@
 import { getPokemon, getPokemonByNum } from '#lib';
-import type {
-	Pokemon,
-	PokemonEnum,
-} from '@favware/graphql-pokemon';
+import type { Pokemon, PokemonEnum } from '@favware/graphql-pokemon';
 import { useState, useCallback } from 'react';
+import { AutoComplete } from './ui/autocomplete';
+// import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 export function SearchBar(props: SearchBarProps) {
 	const [search, setSearch] = useState('');
@@ -12,9 +11,7 @@ export function SearchBar(props: SearchBarProps) {
 		async (e: React.FormEvent<HTMLFormElement>) => {
 			e.preventDefault();
 			const pokemon = isNaN(+search)
-				? await getPokemon(
-						search.toLowerCase() as PokemonEnum
-				  )
+				? await getPokemon(search.toLowerCase() as PokemonEnum)
 				: await getPokemonByNum(+search);
 
 			if (!pokemon) return;
@@ -23,22 +20,34 @@ export function SearchBar(props: SearchBarProps) {
 		[search]
 	);
 
+	const options = [
+		{
+			value: 'abc',
+			label: 'ABC',
+		},
+		{
+			value: 'def',
+			label: 'DEF',
+		},
+	];
+
 	return (
-		<form onSubmit={handleSearch} className="flex w-96">
-			<input
-				type="search"
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
-				placeholder="Search for a Pokémon..."
-				className="w-full rounded-md border-2 border-yellow-400 bg-transparent p-2.5 text-sm text-white caret-white placeholder:text-center placeholder:text-white focus:outline-none"
-			/>
-		</form>
+		<AutoComplete emptyMessage="No results" options={options}
+		// 		className="w-full rounded-md border-2 border-yellow-400 bg-transparent p-2.5 text-sm text-white caret-white placeholder:text-center placeholder:text-white focus:outline-none"
+		/>
+		// <form onSubmit={handleSearch} className="flex w-96">
+		// 	<input
+		// 		type="search"
+		// 		value={search}
+		// 		onChange={(e) => setSearch(e.target.value)}
+		// 		placeholder="Search for a Pokémon..."
+		// 		className="w-full rounded-md border-2 border-yellow-400 bg-transparent p-2.5 text-sm text-white caret-white placeholder:text-center placeholder:text-white focus:outline-none"
+		// 	/>
+		// </form>
 	);
 }
 
 interface SearchBarProps {
 	pokemon: Pokemon | null;
-	setPokemon: React.Dispatch<
-		React.SetStateAction<Pokemon | null>
-	>;
+	setPokemon: React.Dispatch<React.SetStateAction<Pokemon | null>>;
 }
