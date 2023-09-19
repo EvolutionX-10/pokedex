c11e996117469c7439c125884ea6c05bb89bbd06
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-09-19 15:19:31 +0530

    chore: enabled ultra instinct
    permalink: http://whatthecommit.com/0dca0de6bb5225ad23881bf02bcd506f

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,42 @@
 import type { Pokemon } from '@favware/graphql-pokemon';
-import { useState } from 'react';
-import { AutoComplete, AutoCompleteOption } from '@/components/ui/autocomplete';
+import { useEffect, useState } from 'react';
+import { AutoComplete, type Option } from '@/components/autocomplete';
+import { getFuzzyPokemon } from '@/lib/getFuzzyPokemon';
+import { useNavigate } from 'react-router-dom';
 
 export function SearchBar() {
-	const [search, setSearch] = useState('');
-	const [options, setOptions] = useState<AutoCompleteOption[]>([]);
+	const [inputValue, setInputValue] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(true);
+	const [options, setOptions] = useState<Pokemon[]>([]);
+	const [value, setValue] = useState<Option>();
+	const redirect = useNavigate();
+
+	useEffect(() => {
+		if (inputValue && inputValue.length < 2) return;
+		const set = async () => {
+			const pkmns = await getFuzzyPokemon(inputValue || 'pikachu');
+			setOptions(pkmns);
+			setLoading(false);
+		};
+		set();
+	}, [inputValue]);
+
+
+	useEffect(() => {
+		if (!value) return;
+		redirect(`/${value.value}`);
+	}, [value])
 
 	return (
-		<AutoComplete options={options} setOptions={setOptions} search={search} setSearch={setSearch} />
+		<AutoComplete
+			inputValue={inputValue}
+			setInputValue={setInputValue}
+			isLoading={loading}
+			emptyMessage='No Pokemon found'
+			placeholder="Search for a Pokemon"
+			options={options.map((pkmn) => ({ value: pkmn.num.toString(), label: pkmn.species }))}
+			value={value}
+			onValueChange={setValue}
+		/>
 	);
 }
